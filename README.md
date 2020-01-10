98382d0bd88860c50de4bb1efa15455950120517
midori <cleoold@gmail.com>
2020-01-09 23:56:23 -0500

    ok

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# open-to-get-random-image
-A simple server to let a randomly chosen image show up on the page
+[![License: CC0-1.0](https://img.shields.io/badge/License-CC0%201.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)
+
+# Random image displayer
+A simple server to let a randomly chosen image show up on the page when requested. The image pool can be either provided locally in a folder or by URIs.
+
+### Usage:
+#### MODE OPTIONS (CHOOSE ONE FROM TWO):
+ `--directory -d`           will serve every image in the directory  
+ `--file -d`                the file contains URIs to the image, separated by lines. will serve images targeted by links contained in the file
+
+#### REQUIRED OPTION:
+ `--source-path -s <name>`  the source directory/file name provided for above two options
+
+#### OPTIONAL OPTION:
+ `--port -p <number>`       specifies a port number. default 8080
+
+### Example
+* There is a folder called `rresource` parallel to `server.ts`. The folder  contains `1.jpg`, `2.jpg` and so on. In this case run
+  ```sh
+  ts-node server.ts -d -s ./rresource 
+  ```
+* There is a file called `images.txt` in `./rrecource/` which is parallel to `server.ts`. The file contains:
+  ```
+  https://foo.bar/1.jpg
+  https://foo.gar/3.png
+  ...
+  ```
+  In this case run
+  ```sh
+  ts-node server.ts -f -s ./rresource/images.txt 
+  ```
+Enter `localhost:8080` in the browser and the page will show up.
+
+### Install
+```sh
+npm install
+```
+
+### Attribution
+command-line-args (https://www.npmjs.com/package/command-line-args)  
+Express (https://www.npmjs.com/package/express)
+
--- server.ts
+++ server.ts
@@ -1,11 +1,11 @@
-import * as http from 'http';
 import * as fs from 'fs';
+import * as path from 'path';
 
 import * as commandArgs from 'command-line-args';
 
 /* usage page */
 
-const usage: string = 
+const usage: string =
 `A random image server.
 Sends a random image to users when they click your site. The images you provide can be either from your local folder, or URIs.
 
@@ -14,13 +14,17 @@ MODE OPTIONS (CHOOSE ONE FROM TWO):
  --file -d                the file contains URIs to the image, separated by lines. will serve images targeted by links contained in the file
 
 REQUIRED OPTION:
- --source-path -p <name>  the source directory/file name provided for above two options`;
+ --source-path -s <name>  the source directory/file name provided for above two options
+
+OPTIONAL OPTION:
+ --port -p <number>       specifies a port number. default 8080`;
 
 const optionDefinitions = [
-    { name: 'help',         alias: 'h' },
-    { name: 'directory',    alias: 'd' },
-    { name: 'file',         alias: 'f'},
-    { name: 'source-path',  alias: 'p', type: (fn: string) => new FileDetails(fn) }
+    { name: 'help', alias: 'h' },
+    { name: 'directory', alias: 'd' },
+    { name: 'file', alias: 'f' },
+    { name: 'source-path', alias: 's', type: (fn: string) => new FileDetails(fn) },
+    { name: 'port', alias: 'p', type: Number }
 ];
 
 /* utilities */
@@ -28,7 +32,7 @@ const optionDefinitions = [
 class FileDetails {
     filename: string;
     exist: boolean;
-    constructor (filename: string) {
+    constructor(filename: string) {
         this.filename = filename;
         this.exist = ((fn) => {
             try {
@@ -42,22 +46,27 @@ class FileDetails {
     }
 }
 
+function fmtTime(): string {
+    const time = new Date();
+    return `[${time.getMonth()+1}.${time.getDate()} ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}]`;
+}
+
 /* global variables */
 
-let outputLog                       = console.log.bind(this);
-let exitDueToError: boolean         = false;
-let mode: 'folder' | 'uris'         = 'folder';
-let path: string                    = 'dummy';
-let uris: string[]                  = [];
+let outputLog                         = console.log.bind(this);
+let outputErr                         = console.error.bind(this);
+let exitDueToError: boolean           = false;
+let mode: 'folder' | 'uris'           = 'folder';
+let servePath: string                 = '/dummy';
+const mockServePath: string           = '/rimages'; /* for static chosen image resource mock path, only used in directory mode */
+let imgUris: string[]                 = [];
+
+let port: number                      = 8080;
 
 /* parsing command line arguments */
 
 try {
     const commandArguments = commandArgs(optionDefinitions);
-    if (commandArguments === {}) {
-        outputLog('Type --help for usage.');
-        exitDueToError = true;
-    }
     if ('help' in commandArguments) {
         outputLog(usage);
         exitDueToError = true;
@@ -68,12 +77,24 @@ try {
         exitDueToError = true;
     }
     if (!('source-path' in commandArguments) || commandArguments['source-path'] === null || !commandArguments['source-path'].exist) {
-        outputLog('Requires a valid folder/file name.')
+        outputLog('--source-path: Requires a valid folder/file name.')
         exitDueToError = true;
     }
     if (!exitDueToError) { // otherwise program quits
         mode = 'directory' in commandArguments ? 'folder' : 'uris';
-        path = commandArguments['source-path'].filename;
+        servePath = commandArguments['source-path'].filename;
+    }
+    if ('port' in commandArguments) {
+        if (commandArguments['port'] === null) {
+            outputLog('--port: Requires valid port number.');
+            exitDueToError = true;
+        } else {
+            port = commandArguments['port'];
+        }
+    }
+    if (Object.entries(commandArguments).length === 0 && commandArguments.constructor === Object) {
+        outputLog('Type --help for usage.');
+        exitDueToError = true;
     }
 } catch (err) {
     outputLog(err.message);
@@ -90,13 +111,39 @@ if (mode == 'folder') {
         fn = fn.toLowerCase();
         return fn.endsWith('jpg') || fn.endsWith('jpeg') || fn.endsWith('png') || fn.endsWith('gif') || fn.endsWith('bmp') || fn.endsWith('svg');
     }
-    uris = fs.readdirSync(path).filter(isImg).map(fn => path + fn);
+    imgUris = fs.readdirSync(servePath).filter(isImg).map(fn => path.join(mockServePath, fn));
 } else if (mode == 'uris') {
-    uris = fs.readFileSync(path).toString().split('\n');
+    imgUris = fs.readFileSync(servePath).toString().split('\n');
 }
-outputLog(uris)
+
+/* load html template to memory */
+
+const imgSiteTemplateString = fs.readFileSync('template.htm').toString();
 
 /* start server */
 
+import * as express from 'express';
 
+const app = express();
+
+if (mode == 'folder')
+    app.use(mockServePath, express.static(path.join(__dirname, servePath)));
+// ^ if required to use other kind of static resources add other entries similar
+
+app.get('/', (req, res) => {
+    let chosen = imgUris[Math.floor(Math.random() * imgUris.length)];
+    if (mode == 'folder')
+        chosen = '.' + chosen;
+    res.set('Content-Type', 'text/html')
+        .send(Buffer.from(imgSiteTemplateString
+                .replace('%{randomized-image}', chosen)))
+    outputLog(`${fmtTime()} Processed index request image chosen: ${chosen}`);
+})
+
+app.get('*', (req, res) => {
+    res.status(404).send('what???');
+    outputLog(`${fmtTime()} Processed 404 uri: ${req.url}`);
+});
+
+app.listen(port, () => outputLog(`App listening on port ${port}!`))
 
